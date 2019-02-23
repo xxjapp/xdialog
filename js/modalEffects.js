cb9ca0ce72dd6fa47f37fec926bdf293ada86509
Xia Xionjun <xxjapp@gmail.com>
2019-02-23 18:23:39 +0900

    adjust effect rotate

--- js/modalEffects.js
+++ js/modalEffects.js
@@ -1,21 +1,10 @@
-/**
- * modalEffects.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
-var ModalEffects = (function() {
+'use strict';
 
+(function() {
     function init() {
-
         var overlay = document.querySelector('.md-overlay');
 
         [].slice.call(document.querySelectorAll('.md-trigger')).forEach(function(el, i) {
-
             var modal = document.querySelector('#' + el.getAttribute('data-modal')),
                 close = modal.querySelector('.md-close');
 
@@ -47,11 +36,8 @@ var ModalEffects = (function() {
                 ev.stopPropagation();
                 removeModalHandler();
             });
-
         });
-
     }
 
     init();
-
 })();
