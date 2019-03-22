c60154940ed2c848aee91288c4750e89f020cacb
xia <xiaxiongjun@bit-ware.co.jp>
2019-03-22 10:23:24 +0900

    move demo source

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@
     });
 
     document.getElementById('button-demo').addEventListener('click', function() {
-        xdialog.demo();
+        openDialogDemo();
     });
 
     document.getElementById('button-demo-source').addEventListener('click', function() {
@@ -39,7 +39,7 @@
                 }
             },
             beforeshow: function() {
-                let source = xdialog.demo.toString().split('\n').slice(1, -1);
+                let source = openDialogDemo.toString().split('\n').slice(1, -1);
                 let spaceWidth = source[0].match(/^ */)[0].length;
 
                 source = source.map(function(line) {
@@ -58,4 +58,38 @@
             }
         });
     });
+
+    function openDialogDemo() {
+        xdialog.open({
+            title: 'Login Demo',
+            body: '\
+            <style>\
+                .demo-mb-1 { margin-bottom: 1em; }\
+                .demo-row { text-align: center; }\
+                .demo-row label { min-width: 6em; display: inline-block; }\
+                .demo-row input { padding: 0.3em; outline: none; min-width: 12em; }\
+                .demo-validated input { border: green 2px solid; }\
+                .demo-validated input:invalid { border: red 2px solid; }\
+            </style>\
+            <div id="demo-form">\
+                <div class="demo-row demo-mb-1"><label for="uname">User Name</label><input type="text" id="uname" required></div>\
+                <div class="demo-row"><label for="psw">Password</label><input type="password" id="psw" required></div>\
+            </div>',
+            buttons: { ok: 'Login', cancel: 'Cancel' },
+            style: 'width: auto',
+            effect: '3d_rotate_bottom',
+            onok: function() {
+                document.getElementById('demo-form').classList.add('demo-validated');
+
+                let uname = document.getElementById('uname').value;
+                let psw = document.getElementById('psw').value;
+
+                if (!uname || !psw) {
+                    return false;
+                }
+
+                xdialog.alert('Welcome, ' + uname);
+            }
+        });
+    }
 })();
--- xdialog/xdialog.js
+++ xdialog/xdialog.js
@@ -742,40 +742,6 @@ window.xdialog = function() {
         return spinOverlayElement;
     }
 
-    function demo() {
-        xdialog.open({
-            title: 'Login Demo',
-            body: '\
-            <style>\
-                .demo-mb-1 { margin-bottom: 1em; }\
-                .demo-row { text-align: center; }\
-                .demo-row label { min-width: 6em; display: inline-block; }\
-                .demo-row input { padding: 0.3em; outline: none; min-width: 12em; }\
-                .demo-validated input { border: green 2px solid; }\
-                .demo-validated input:invalid { border: red 2px solid; }\
-            </style>\
-            <div id="demo-form">\
-                <div class="demo-row demo-mb-1"><label for="uname">User Name</label><input type="text" id="uname" required></div>\
-                <div class="demo-row"><label for="psw">Password</label><input type="password" id="psw" required></div>\
-            </div>',
-            buttons: { ok: 'Login', cancel: 'Cancel' },
-            style: 'width: auto',
-            effect: '3d_rotate_bottom',
-            onok: function() {
-                document.getElementById('demo-form').classList.add('demo-validated');
-
-                let uname = document.getElementById('uname').value;
-                let psw = document.getElementById('psw').value;
-
-                if (!uname || !psw) {
-                    return false;
-                }
-
-                xdialog.alert('Welcome, ' + uname);
-            }
-        });
-    }
-
     return {
         // xdialog.init(options)
         // initialize xdialog
@@ -808,9 +774,5 @@ window.xdialog = function() {
         // xdialog.stopSpin()
         // stop spin animation
         stopSpin: stopSpin,
-
-        // xdialog.demo()
-        // show a demo dialog
-        demo: demo,
     };
 }();
