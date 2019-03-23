d5b91e3e232c4c0c013998d856cfa638ba98d87f
Xia Xionjun <xxjapp@gmail.com>
2019-03-24 00:00:39 +0900

    demo to demo1

--- js/index.js
+++ js/index.js
@@ -23,63 +23,31 @@
         });
     });
 
-    document.getElementById('button-demo').addEventListener('click', function() {
-        openDialogDemo();
+    document.getElementById('button-demo1').addEventListener('click', function() {
+        openDialogDemo1();
     });
 
-    document.getElementById('button-demo-source').addEventListener('click', function() {
-        xdialog.open({
-            title: 'demo source',
-            body: '<pre><code class="js" id="demo-source"></code></pre>',
-            style: 'width: auto;',
-            buttons: {
-                ok: {
-                    text: 'copy',
-                    clazz: 'xd-button xd-ok demo-copy-button'
-                }
-            },
-            beforeshow: function() {
-                let source = openDialogDemo.toString().split('\n').slice(1, -1);
-                let spaceWidth = source[0].match(/^ */)[0].length;
-
-                source = source.map(function(line) {
-                    return line.slice(spaceWidth);
-                }).join('\n');
-
-                let sourceElement = document.getElementById('demo-source');
-                sourceElement.innerText = source;
-                hljs.highlightBlock(sourceElement);
-
-                new ClipboardJS('.demo-copy-button', {
-                    text: function() {
-                        return source;
-                    }
-                });
-            }
-        });
-    });
-
-    function openDialogDemo() {
+    function openDialogDemo1() {
         xdialog.open({
             title: 'Login Demo',
             body: '\
             <style>\
-                .demo-mb-1 { margin-bottom: 1em; }\
-                .demo-row { text-align: center; }\
-                .demo-row label { min-width: 6em; display: inline-block; }\
-                .demo-row input { padding: 0.3em; outline: none; min-width: 12em; }\
-                .demo-validated input { border: green 2px solid; }\
-                .demo-validated input:invalid { border: red 2px solid; }\
+                .demo1-mb-1 { margin-bottom: 1em; }\
+                .demo1-row { text-align: center; }\
+                .demo1-row label { min-width: 6em; display: inline-block; }\
+                .demo1-row input { padding: 0.3em; outline: none; min-width: 12em; }\
+                .demo1-validated input { border: green 2px solid; }\
+                .demo1-validated input:invalid { border: red 2px solid; }\
             </style>\
-            <div id="demo-form">\
-                <div class="demo-row demo-mb-1"><label for="uname">User Name</label><input type="text" id="uname" required></div>\
-                <div class="demo-row"><label for="psw">Password</label><input type="password" id="psw" required></div>\
+            <div id="demo1-form">\
+                <div class="demo1-row demo1-mb-1"><label for="uname">User Name</label><input type="text" id="uname" required></div>\
+                <div class="demo1-row"><label for="psw">Password</label><input type="password" id="psw" required></div>\
             </div>',
             buttons: { ok: 'Login', cancel: 'Cancel' },
             style: 'width: auto',
             effect: '3d_rotate_bottom',
             onok: function() {
-                document.getElementById('demo-form').classList.add('demo-validated');
+                document.getElementById('demo1-form').classList.add('demo1-validated');
 
                 let uname = document.getElementById('uname').value;
                 let psw = document.getElementById('psw').value;
@@ -92,6 +60,38 @@
             }
         });
     }
+
+    document.getElementById('button-demo1-source').addEventListener('click', function() {
+        xdialog.open({
+            title: 'demo1 source',
+            body: '<pre><code class="js" id="demo1-source"></code></pre>',
+            style: 'width: auto;',
+            buttons: {
+                ok: {
+                    text: 'copy',
+                    clazz: 'xd-button xd-ok demo1-copy-button'
+                }
+            },
+            beforeshow: function() {
+                let source = openDialogDemo1.toString().split('\n').slice(1, -1);
+                let spaceWidth = source[0].match(/^ */)[0].length;
+
+                source = source.map(function(line) {
+                    return line.slice(spaceWidth);
+                }).join('\n');
+
+                let sourceElement = document.getElementById('demo1-source');
+                sourceElement.innerText = source;
+                hljs.highlightBlock(sourceElement);
+
+                new ClipboardJS('.demo1-copy-button', {
+                    text: function() {
+                        return source;
+                    }
+                });
+            }
+        });
+    });
 
     document.getElementById('button-spin').addEventListener('click', function() {
         xdialog.startSpin();
